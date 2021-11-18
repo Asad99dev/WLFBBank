f22e7c91e387497083121c7af64f41fbb869c2c4
Asad99dev <73398317+Asad99dev@users.noreply.github.com>
2021-11-18 18:03:45 +0000

    Update WLFBState.java

--- src/WLFBState.java
+++ src/WLFBState.java
@@ -7,8 +7,8 @@ public class WLFBState {
 	private WLFBState mySharedObj;
 	private String myThreadName;
 	private double AccountOne, AccountTwo, AccountThree;
-	private boolean accessing = false; // true a thread has a lock, false otherwise
-	private int threadsWaiting = 0; // number of waiting writers
+	private boolean accessing = false; 
+	private int threadsWaiting = 0; 
 
 	WLFBState(double balance) {
 		AccountOne = balance;
@@ -17,27 +17,27 @@ public class WLFBState {
 	}
 
 	public synchronized void acquireLock() throws InterruptedException {
-		Thread me = Thread.currentThread(); // get a ref to the current thread
+		Thread me = Thread.currentThread(); 
 		System.out.println(me.getName() + " is attempting to acquire a lock!");
 		++threadsWaiting;
-		while (accessing) { // while someone else is accessing or threadsWaiting > 0
+		while (accessing) { 
 			System.out.println(me.getName() + " waiting to get a lock as someone else is accessing...");
-			// wait for the lock to be released - see releaseLock() below
+			
 			wait();
 		}
-		// nobody has got a lock so get one
+		
 		--threadsWaiting;
 		accessing = true;
 		System.out.println(me.getName() + " got a lock!");
 	}
 
-	// Releases a lock to when a thread is finished
+	
 
 	public synchronized void releaseLock() {
-		// release the lock and tell everyone
+		
 		accessing = false;
 		notifyAll();
-		Thread me = Thread.currentThread(); // get a ref to the current thread
+		Thread me = Thread.currentThread(); 
 		System.out.println(me.getName() + " released a lock!");
 	}
 
@@ -46,7 +46,7 @@ public class WLFBState {
 		String theOutput = null;
 
 		if (theInput.equalsIgnoreCase("1")) {
-			// Correct request
+			
 
 			int money = 200;
 
@@ -180,12 +180,12 @@ public class WLFBState {
 
 		}
 
-		else { // incorrect request
+		else { 
 			theOutput = myThreadName + " received incorrect request - only understand 1, 2, 3 or 4";
 
 		}
 
-		// Return the output message to the ActionServer
+		
 		System.out.println(theOutput);
 		return theOutput;
 	}
